7eb03f89badaa287cfe6a79285fbdc841d6f0938
Saurabh-101 <saurabhgupta72dh3@gmail.com>
2025-06-12 17:07:09 +0530

    Navbar complete

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,33 @@
 import React from 'react'
-
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Collection from './pages/Collection'
+import About from './pages/About'
+import Orders from './pages/Orders'
+import Product from './pages/Product'
+import Cart from './pages/Cart'
+import Contact from './pages/Contact'
+import Login from './pages/Login'
+import PlaceOrder from './pages/PlaceOrder'
+import Navbar from './components/Navbar'
 const App = () => {
   return (
-    <div><h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1></div>
+    <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />}/>
+        <Route path='/collection' element={<Collection />}/>
+        <Route path='/about' element={<About />}/>
+        <Route path='/contact' element={<Contact />}/>
+        <Route path='/product/:productID' element={<Product />}/>
+        <Route path='/cart' element={<Cart />}/>
+        <Route path='/login' element={<Login />}/>
+        <Route path='/orders' element={<Orders />}/>
+        <Route path='/place-order' element={<PlaceOrder />}/>
+        
+        
+      </Routes>
+    </div>
   )
 }
 
