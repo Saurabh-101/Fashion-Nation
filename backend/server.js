f8fac9cf90d86657fb3d96339a3f13d06d51379e
Saurabh-101 <saurabhgupta72dh3@gmail.com>
2025-06-22 19:38:11 +0530

    Initiated backend

---
+++ backend/server.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import cors from 'cors';
+import 'dotenv/config';
+
+//App config
+
+const app = express();
+
+const port = process.env.PORT || 4000;
+
+//Middlewares
+
+app.use(express.json());
+app.use(cors());
+
+
+//API endpoints
+
+app.get('/',(req,res)=>{
+    res.send('API Working');
+})
+
+app.listen(port,()=>console.log('Server started on port: '+port));
