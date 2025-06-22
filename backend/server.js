833010f1f018e5bb9396abc1d302107bb7bc163f
Saurabh-101 <saurabhgupta72dh3@gmail.com>
2025-06-22 20:12:49 +0530

    config cloudinary connection

--- backend/server.js
+++ backend/server.js
@@ -3,23 +3,23 @@ import cors from 'cors';
 import 'dotenv/config';
 
 import connectDB from './config/mongodb.js';
+import connectCloudinary from './config/cloudinary.js';
 
 //App config
-
 const app = express();
-
 const port = process.env.PORT || 4000;
 
+
 connectDB();
+connectCloudinary();
+
 
 //Middlewares
-
 app.use(express.json());
 app.use(cors());
 
 
 //API endpoints
-
 app.get('/',(req,res)=>{
     res.send('API Working');
 })
