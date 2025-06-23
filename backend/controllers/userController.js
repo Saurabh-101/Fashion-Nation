fdd31771bcb3cd4adf877c9e9bb1444ee3198277
Saurabh-101 <saurabhgupta72dh3@gmail.com>
2025-06-23 22:46:43 +0530

    Created adminAuth middleware and added in productRoutes for add & remove products, used jwt token to verify admin and let access to product routes

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -116,6 +116,38 @@ const registerUser = async (req, res) => {
 // Admin login route
 const adminLogin = async(req, res) => {
 
+    try {
+        
+        const {email, password } = req.body;
+        if(!validator.isEmail(email)){
+            res.json({
+                success:false,
+                msg:"Invalid email address."
+            })
+        }
+        if(email === process.env.ADMIN_EMAIL && password === process.env.ADMIN_PASSWORD){
+            const token = jwt.sign(email+password, process.env.JWT_SECRET);
+            res.json({
+                success:true,
+                token
+            })
+        }else{
+            res.json({
+                success:false,
+                msg:"Invalid credentials."
+            })
+        }
+
+
+    } catch (error) {
+        console.log(error);
+        res.json({
+            success:false,
+            msg: error.message
+        })
+        
+    }
+
 }
 
 export {loginUser, registerUser, adminLogin} 
