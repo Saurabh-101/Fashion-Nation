99b51497460fa44fe1817d67b6db973302792fc3
Saurabh-101 <saurabhgupta72dh3@gmail.com>
2025-06-23 10:29:48 +0530

    Register function is done >> controllers/userController.js

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,3 +1,13 @@
+import jwt from "jsonwebtoken";
+import userModel from "../models/userModel.js";
+import validator from "validator";
+import bcrypt from "bcrypt";
+
+const createToken = (id) =>{
+
+    return jwt.sign({id},process.env.JWT_SECRET);
+}
+
 
 // Route for user login
 const loginUser = async (req, res) => {
@@ -11,9 +21,62 @@ const loginUser = async (req, res) => {
 // Route for user register user
 const registerUser = async (req, res) => {
 
-    res.json({
-        msg:"Register API working"
-    })
+    try {
+        
+        const {name, email, password} = req.body;
+        
+        // check if user already exist or not
+        
+        const exists = await userModel.findOne({email});
+
+        if(exists){
+            return res.json({
+                success: false,
+                msg:"User already exist"
+            });
+        }
+        
+        // Validating email format and strong password
+        if(!validator.isEmail(email)){
+            return res.json({
+                success: false,
+                msg:"Invalid email address."
+            })
+        }
+        if(password.length<8){
+            return res.json({
+                success:false,
+                msg:"Enter a strong Password"
+            })
+        }
+
+        // Hashing user password
+        const salt = await bcrypt.genSalt(11);
+
+        const hashedPassword = await bcrypt.hash(password,salt);
+
+        const newUser = new userModel({
+            name,
+            email,
+            password:hashedPassword
+        })
+
+        const user = await newUser.save();
+
+        const token = createToken(user._id);
+
+        res.json({
+            success: true,
+            token
+        })
+
+    } catch (error) {
+        console.log(error);
+        res.json({
+            success:false,
+            msg:error.message
+        })
+    }
 
 }
 
