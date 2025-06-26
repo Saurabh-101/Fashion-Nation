fef4811d6715086e9bd495925739c25fddb97253
Saurabh-101 <saurabhgupta72dh3@gmail.com>
2025-06-26 19:52:29 +0530

    Created controller to add cart data to user cartData object and fixed a typo in userModel <cartDate --> cartData>

--- backend/controllers/cartController.js
+++ backend/controllers/cartController.js
@@ -1,13 +1,43 @@
+import userModel from "../models/userModel.js";
 
 
 // Add products to userCart
-
 const addToCart = async(req,res)=>{
 
-    res.json({
-        msg:"checking auth middleware functioning."
-    })
+    try {
+        
+        const {userId, itemId, size} = req.body;
 
+        const userData = await userModel.findById(userId);
+        let cartData = await userData.cartData;
+
+        if(cartData[itemId]){
+            if(cartData[itemId][size]){
+                cartData[itemId][size] += 1;
+            }else{
+                cartData[itemId][size] = 1;
+            }
+        }else{
+            cartData[itemId] ={}
+            cartData[itemId][size] = 1;
+        }
+
+        await userModel.findByIdAndUpdate(userId, {cartData});
+
+        res.json({
+            success:true,
+            msg:"Added to cart."
+        })
+
+    } catch (error) {
+        console.log(error);
+        res.json({
+            success:false,
+            msg:error.message
+        })
+        
+    }
+    
 }
 
 // update userCart
