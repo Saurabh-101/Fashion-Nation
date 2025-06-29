40bc8ff8ff24bb68878df9f38a98e1bb8e7b05fd
Saurabh-101 <saurabhgupta72dh3@gmail.com>
2025-06-29 12:52:19 +0530

    Fixed sub-category filter bug

--- frontend/src/pages/Collection.jsx
+++ frontend/src/pages/Collection.jsx
@@ -104,13 +104,13 @@ const Collection = () => {
             <p className='mb-3 text-sm font-medium'>TYPE</p>
             <div className='flex flex-col gap-2 text-sm font-light text-gray-700'>
               <p className='flex gap-2'>
-                <input onChange={toggleSubCategory} className='w-3' type="checkbox" value={'Topwear'}/> Topwear
+                <input onChange={toggleSubCategory} className='w-3' type="checkbox" value={'topwear'}/> Topwear
               </p>
               <p className='flex gap-2'>
-                <input onChange={toggleSubCategory} className='w-3' type="checkbox" value={'Bottomwear'}/> Bottomwear
+                <input onChange={toggleSubCategory} className='w-3' type="checkbox" value={'bottomwear'}/> Bottomwear
               </p>
               <p className='flex gap-2'>
-                <input onChange={toggleSubCategory} className='w-3' type="checkbox" value={'Winterwear'}/> Winterwear
+                <input onChange={toggleSubCategory} className='w-3' type="checkbox" value={'winterwear'}/> Winterwear
               </p>
             </div>
           </div>
